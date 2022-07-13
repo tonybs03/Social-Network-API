f800793f5962c567a861a394b7dd7771674f6c94
Tony Fan <tony.bs.0303@gmail.com>
2022-07-13 18:16:48 -0400

    server seems to be running on 3001 properly, Mongo seems to have creaated socialDB

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,39 @@
 const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
+
+// create reactionschema based on readme
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdat => moment(createdat).format('MMM DD, YYYY hh:mm a')  
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true, //virtuals enabled
+      getters: true   //getters enabled
+    },
+    id: false
+  }
+);
+
+
 // thought schema based on readme
 const thoughtSchema = new Schema(
   {
@@ -40,36 +73,7 @@ thoughtSchema
   });
 
 
-// create reactionschema based on readme
-const reactionSchema = new Schema(
-  {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxlength: 280,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: createdat => moment(createdat).format('MMM DD, YYYY hh:mm a')  
-    },
-  },
-  {
-    toJSON: {
-      virtuals: true, //virtuals enabled
-      getters: true   //getters enabled
-    },
-    id: false
-  }
-);
+
 
 
 const Thought = model('thought', thoughtSchema);
