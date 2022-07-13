b4e234790a445373d336928fdcdecbe214ead2b2
Tony Fan <tony.bs.0303@gmail.com>
2022-07-13 16:03:26 -0400

    first commit, initialized basic template based on student mini project

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+// const assignmentSchema = require('./Assignment');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/, 'Invalid email format'],
+    },
+    github: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    assignments: [assignmentSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', studentSchema);
+
+module.exports = User;
